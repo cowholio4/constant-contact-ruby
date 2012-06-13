58b838c6a7847bcb9519d6753a953cb1768fd53e
Jason Pope <cowholio4@gmail.com>
2012-06-12 22:23:43 -0700

    removing user, password and api key to get ready for oauth2

--- lib/constant_contact.rb
+++ lib/constant_contact.rb
@@ -14,12 +14,9 @@ module ConstantContact
     headers 'Accept'        => 'application/atom+xml'
     headers 'Content-Type'  => 'application/atom+xml'
     
-    API_KEY = "59ca4bb4-51e9-4c08-a2b2-a34aac7bb78f"
-
     class << self
       # Create a connection to the Constant Contact API using your login credentials
-      def setup( user, pass )
-        basic_auth "#{API_KEY}%#{user}", pass
+      def setup( user )
         base_uri "https://api.constantcontact.com/ws/customers/#{user.downcase}"
       end
     end
